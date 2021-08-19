01a908b448a358db49e7958b9dda1d500f37a4ec
ElectronicGomez <89115992+ElectronicGomez@users.noreply.github.com>
2021-08-18 22:50:35 -0500

    Add files via upload

---
+++ Practica1/Practica Calificada 1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Practica_Calificada_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(417, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Usar Joystick";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(5, 34);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 335);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(425, 5);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(285, 160);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(425, 198);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(285, 186);
+            this.listBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 314);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Practica1/Practica Calificada 1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Joystick;
+
+namespace Practica_Calificada_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        InfoJoystick j0 = new InfoJoystick(0);
+        InfoJoystick j1 = new InfoJoystick(1);
+        InfoJoystick j2 = new InfoJoystick(2);
+        Graphics g1;
+        int px, py, pz, pr, x, y, z, r;
+
+        int Alto;
+        int Ancho;
+        double aux1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Alto = pictureBox1.Height;  // Altura del PictureBox
+            Ancho = pictureBox1.Width;  // Ancho del PictureBox
+            g1 = pictureBox1.CreateGraphics();
+            g1.Clear(Color.White);           // Color de Fondo
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int Grosor = 20;             // Ancho del Brazo
+            int Largo = 100;                    // Largo del Brazo
+
+            int[] d = j0.GetData();
+            string[] ds = j0.GetDataString();
+
+            //listBox1.DataSource = d;
+            //listBox2.DataSource = ds;
+
+            px = d[0]; py = d[1]; //pz = d[2]; pr = d[3];
+            x = (pz * Ancho) / 65535;
+            y = (pr * Alto) / 65535;
+            g1.Clear(Color.White);
+            g1.FillEllipse(Brushes.Blue, x - 20, y - 20, 40, 40);
+
+            // Primer Grafico
+            // Linea Centro
+            Point[] ls1 = new Point[2];
+            ls1[0] = new Point(x, y);
+            ls1[1] = new Point(Ancho / 2, Alto / 2);
+            Pen pls1 = new Pen(Color.Blue);
+            pls1.Width = 1;
+            g1.DrawLines(pls1, ls1);
+
+            // Linea Inferior
+            Point[] li1 = new Point[2];
+            li1[0] = new Point(Ancho / 2 - Largo, Alto / 2 + Grosor);
+            li1[1] = new Point(Ancho / 2, Alto / 2 + Grosor);
+            Pen pli1 = new Pen(Color.Blue);
+            pli1.Width = 1;
+            g1.DrawLines(pli1, li1);
+
+           
+           //CENTRO
+            Rectangle c1 = new Rectangle(Ancho/2-20, Alto/2-20, 40, 40);
+            /*Pen pc1 = new Pen(Color.Purple);
+            pc1.Width = 1;
+            g1.DrawEllipse(pc1, c1);
+            */
+            g1.FillEllipse(Brushes.Blue, c1);
+
+  
+            // Circulo
+
+            Rectangle c2 = new Rectangle(0,0,Ancho, Alto);
+            Pen pc2 = new Pen(Color.Purple);
+            pc2.Width = 1;
+            g1.DrawEllipse(pc2, c2);
+
+            // Circulo 2 
+            Rectangle c3 = new Rectangle((int)(Ancho / 2 - 20+aux1), 0, 40, 40);
+            g1.FillEllipse(Brushes.Red, c3);
+            if (px>32511)
+            {
+                aux1=aux1+0.1;
+            }
+            if (px< 32511)
+            {
+                aux1 = aux1 - 0.1;
+            }
+            if (px == 32511)
+            {
+                aux1 = 0;
+            }
+            
+            Pen sal = new Pen(Color.Red);
+            g1.DrawLine(sal, Ancho / 2, Alto / 2, Ancho/2+(int)(Ancho/2 * Math.Cos(aux1)), Alto/2+(int)(Alto/2 * Math.Sin(aux1)));
+
+
+            Rectangle c4 = new Rectangle(Ancho / 2 + (int)(Ancho / 2 * Math.Cos(aux1))-20, Alto / 2 + (int)(Alto / 2 * Math.Sin(aux1))-20, 40, 40);
+            g1.FillEllipse(Brushes.Green, c4);
+        }
+    }
+}
