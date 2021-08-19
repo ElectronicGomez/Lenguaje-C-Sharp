e7feaf23ab2145321bcc948a8ebe0f91aab84f56
ElectronicGomez <89115992+ElectronicGomez@users.noreply.github.com>
2021-08-18 22:44:11 -0500

    Add files via upload

---
+++ Ejercicios/reloj de arena/Tarea 5/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Tarea_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.a1 = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.TextBox();
+            this.a2 = new System.Windows.Forms.TextBox();
+            this.b2 = new System.Windows.Forms.TextBox();
+            this.a3 = new System.Windows.Forms.TextBox();
+            this.b3 = new System.Windows.Forms.TextBox();
+            this.H1 = new System.Windows.Forms.TextBox();
+            this.H2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(566, 30);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(54, 21);
+            this.a1.TabIndex = 1;
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(566, 60);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(54, 21);
+            this.b1.TabIndex = 2;
+            // 
+            // a2
+            // 
+            this.a2.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.Location = new System.Drawing.Point(566, 105);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(54, 21);
+            this.a2.TabIndex = 3;
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(566, 135);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(54, 21);
+            this.b2.TabIndex = 4;
+            // 
+            // a3
+            // 
+            this.a3.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.Location = new System.Drawing.Point(566, 181);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(54, 21);
+            this.a3.TabIndex = 5;
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(566, 211);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(54, 21);
+            this.b3.TabIndex = 6;
+            // 
+            // H1
+            // 
+            this.H1.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.H1.Location = new System.Drawing.Point(674, 31);
+            this.H1.Name = "H1";
+            this.H1.Size = new System.Drawing.Size(54, 21);
+            this.H1.TabIndex = 7;
+            // 
+            // H2
+            // 
+            this.H2.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.H2.Location = new System.Drawing.Point(674, 69);
+            this.H2.Name = "H2";
+            this.H2.Size = new System.Drawing.Size(54, 21);
+            this.H2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(524, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "a1 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(524, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "b1 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(524, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "a2 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(524, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "a3 :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(524, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "b2 :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(524, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "b3 :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(629, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "H1 :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(629, 72);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "H2 :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(649, 153);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 17;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(651, 116);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 16);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "porcentaje";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(600, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 29);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "GRAFICAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(600, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 29);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "LLENADO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(469, 393);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 422);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.H2);
+            this.Controls.Add(this.H1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox a1;
+        private System.Windows.Forms.TextBox b1;
+        private System.Windows.Forms.TextBox a2;
+        private System.Windows.Forms.TextBox b2;
+        private System.Windows.Forms.TextBox a3;
+        private System.Windows.Forms.TextBox b3;
+        private System.Windows.Forms.TextBox H1;
+        private System.Windows.Forms.TextBox H2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Ejercicios/reloj de arena/Tarea 5/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        int w, h, a11, b11, a22, b22, a33, b33, H11, H22, counth1=0, counth2=0;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            llenado = 0;
+            counth1 = 0;
+            counth2 = 0;
+         
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        double xa, xb;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("10");
+            comboBox1.Items.Add("20");
+            comboBox1.Items.Add("30");
+            comboBox1.Items.Add("40");
+            comboBox1.Items.Add("50");
+            comboBox1.Items.Add("60");
+            comboBox1.Items.Add("70");
+            comboBox1.Items.Add("80");
+            comboBox1.Items.Add("90");
+            comboBox1.Items.Add("100");
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //pen para la simulacion 
+            Pen p2 = new Pen(Color.Aqua);
+            porcentaje = Convert.ToInt32(comboBox1.SelectedItem) * HT / 100;
+            p2.Width = 5;
+            
+            //calcular las nuevas elipses que hacen posible la simulacion
+            llenado = llenado + 1;
+           
+
+            
+
+            xa = (H11 - counth1) * (a22 - a11) / H11 + a11;
+            xb = (H11 - counth1) * (b22 - b11) / H11 + b11;
+            //g.DrawEllipse(p2, w / 2 - Convert.ToInt32(xa), h / 2 - Convert.ToInt32(xb) - counth2 + H22, 2 * Convert.ToInt32(xa), 2 * Convert.ToInt32(xb));
+            g.FillEllipse(Brushes.Yellow, w / 2 - Convert.ToInt32(xa), h / 2 - Convert.ToInt32(xb) - counth1, 2 * Convert.ToInt32(xa), 2 * Convert.ToInt32(xb));
+            counth1 = counth1 + 1;
+
+            Point[] pts = new Point[3];
+            pts[0] = new Point(w / 2 - a11, h / 2 - H11);
+            pts[1] = new Point(w / 2 - a22, h / 2);
+            pts[2] = new Point(w / 2 - a33, h / 2 + H22);
+            //izquierda
+            Point[] pts1 = new Point[3];
+            pts1[0] = new Point(w / 2 + a11, h / 2 - H11);
+            pts1[1] = new Point(w / 2 + a22, h / 2);
+            pts1[2] = new Point(w / 2 + a33, h / 2 + H22);
+
+            //pen para el dibujo y los trazos
+            Pen p1 = new Pen(Color.Black);
+            p1.Width = 2;
+            //Dibujo las elipses
+            g.DrawEllipse(p1, w / 2 - a11, h / 2 - b11 - H11, 2 * a11, 2 * b11);
+            g.DrawEllipse(p1, w / 2 - a22, h / 2 - b22, 2 * a22, 2 * b22);
+            g.DrawEllipse(p1, w / 2 - a33, h / 2 - b33 + H22, 2 * a33, 2 * b33);
+            //Dibujo las lineas que las unen
+            g.DrawLines(p1, pts);
+            g.DrawLines(p1, pts1);
+          
+
+            if ((llenado - porcentaje) == 0)
+            {
+                timer2.Enabled = false;
+            }
+
+            if ((H11 - counth1) == 0)
+                timer2.Enabled = false;
+        }
+        int llenado, porcentaje,HT;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //pen para la simulacion 
+            Pen p2 = new Pen(Color.Yellow);
+            p2.Width = 2;
+
+            porcentaje = Convert.ToInt32(comboBox1.SelectedItem) * HT / 100;
+
+            //calcular las nuevas elipses que hacen posible la simulacion
+
+            xa = (H22 - counth2) * (a33 - a22) / H22 + a22;
+            xb = (H22 - counth2) * (b33 - b22) / H22 + b22;
+            //g.DrawEllipse(p2, w / 2 - Convert.ToInt32(xa), h / 2 - Convert.ToInt32(xb) - counth2 + H22, 2 * Convert.ToInt32(xa), 2 * Convert.ToInt32(xb));
+            g.FillEllipse(Brushes.Yellow, w / 2 - Convert.ToInt32(xa), h / 2 - Convert.ToInt32(xb) - counth2 + H22, 2 * Convert.ToInt32(xa), 2 * Convert.ToInt32(xb));
+            counth2 = counth2 + 1;
+            llenado = llenado + 1;
+            
+
+            Point[] pts = new Point[3];
+            pts[0] = new Point(w / 2 - a11, h / 2 - H11);
+            pts[1] = new Point(w / 2 - a22, h / 2);
+            pts[2] = new Point(w / 2 - a33, h / 2 + H22);
+            //izquierda
+            Point[] pts1 = new Point[3];
+            pts1[0] = new Point(w / 2 + a11, h / 2 - H11);
+            pts1[1] = new Point(w / 2 + a22, h / 2);
+            pts1[2] = new Point(w / 2 + a33, h / 2 + H22);
+
+            //pen para el dibujo y los trazos
+            Pen p1 = new Pen(Color.Black);
+            p1.Width = 2;
+            //Dibujo las elipses
+            g.DrawEllipse(p1, w / 2 - a11, h / 2 - b11 - H11, 2 * a11, 2 * b11);
+            g.DrawEllipse(p1, w / 2 - a22, h / 2 - b22, 2 * a22, 2 * b22);
+            g.DrawEllipse(p1, w / 2 - a33, h / 2 - b33 + H22, 2 * a33, 2 * b33);
+            //Dibujo las lineas que las unen
+            g.DrawLines(p1, pts);
+            g.DrawLines(p1, pts1);
+
+           
+
+            if ((llenado - porcentaje) == 0)
+            {
+                timer1.Enabled = false;
+            }
+
+
+            if ((H22-counth2)==0)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = true;
+            }
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            llenado = 0;
+          
+            
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+            w = pictureBox1.Size.Width;
+            h = pictureBox1.Size.Height;
+            a11 = Convert.ToInt32(a1.Text);
+            b11 = Convert.ToInt32(b1.Text);
+            a22 = Convert.ToInt32(a2.Text);
+            b22 = Convert.ToInt32(b2.Text);
+            a33 = Convert.ToInt32(a3.Text);
+            b33 = Convert.ToInt32(b3.Text);
+            H11 = Convert.ToInt32(H1.Text);
+            H22 = Convert.ToInt32(H2.Text);
+            HT = H11 + H22;
+            porcentaje = Convert.ToInt32(comboBox1.SelectedItem) * HT / 100;
+            //lineas para cerrar la figuras
+            //derecha
+            Point[] pts = new Point[3];
+            pts[0] = new Point(w / 2 - a11, h / 2 - H11);
+            pts[1] = new Point(w / 2 - a22, h / 2);
+            pts[2] = new Point(w / 2 - a33, h / 2 + H22);
+            //izquierda
+            Point[] pts1 = new Point[3];
+            pts1[0] = new Point(w / 2 + a11, h / 2 - H11);
+            pts1[1] = new Point(w / 2 + a22, h / 2);
+            pts1[2] = new Point(w / 2 + a33, h / 2 + H22);
+
+            //pen para el dibujo y los trazos
+            Pen p1 = new Pen(Color.Black);
+            p1.Width = 2;
+            //Dibujo las elipses
+            g.DrawEllipse(p1, w / 2 - a11, h / 2 - b11 - H11, 2 * a11, 2 * b11);
+            g.DrawEllipse(p1, w / 2 - a22, h / 2 - b22, 2 * a22, 2 * b22);
+            g.DrawEllipse(p1, w / 2 - a33, h / 2 - b33 + H22, 2 * a33, 2 * b33);
+            //Dibujo las lineas que las unen
+            g.DrawLines(p1, pts);
+            g.DrawLines(p1,pts1);
+
+        }
+    }
+}
